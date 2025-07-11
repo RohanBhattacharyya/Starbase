f7d5bdc9104ea3a150cd431121dbd630e37b18bc
Your Name <you@example.com>
2025-07-10 22:50:40 -0700

    fix: Resolve 'require is not defined' in input dialog renderer

--- preload.js
+++ preload.js
@@ -16,7 +16,11 @@ contextBridge.exposeInMainWorld('electronAPI', {
   deleteInstance: (instanceName) => ipcRenderer.invoke('delete-instance', instanceName),
   deleteMod: (instanceName, modId) => ipcRenderer.invoke('delete-mod', instanceName, modId),
   checkForOpenstarboundUpdate: () => ipcRenderer.invoke('check-for-openstarbound-update'),
-  openInputDialog: (options) => ipcRenderer.invoke('open-input-dialog', options),
-  // New: Expose a way for the input dialog to send its response back
-  sendDialogResponse: (response) => ipcRenderer.send('dialog-response', response)
+  openInputDialog: (options) => ipcRenderer.invoke('open-input-dialog', options)
+});
+
+// Expose specific IPC methods for the input dialog renderer
+contextBridge.exposeInMainWorld('inputDialogAPI', {
+  sendResponse: (response) => ipcRenderer.send('dialog-response', response),
+  onSetOptions: (callback) => ipcRenderer.on('set-dialog-options', callback)
 });
